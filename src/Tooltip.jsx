97a033e18c9eae2218d2c9ec07897f7a470fbff6
Abe Handler <akh2103@gmail.com>
2020-02-23 13:28:50 -0500

    just backup

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -5,10 +5,11 @@ var d3 = require('d3');
 export default class Tooltip extends React.Component{
 
     render(){
+        let y = 15;
         return(
             <g>
-              <rect x={this.props.x} y={5} opacity={1} stroke="grey" strokeWidth="2" height={40} width={110} fill="white"/>
-              <text style={{backgroundColor: "white"}} x={this.props.x} y={5 + 20} opacity={1} height="10" width="23" fill="black">
+              <rect x={this.props.x} y={y} opacity={1} stroke="grey" strokeWidth="2" height={40} width={110} fill="white"/>
+              <text style={{backgroundColor: "white"}} x={this.props.x} y={y + 20} opacity={1} height="10" width="23" fill="black">
               blah
               </text>
               </g>
